7ab6cdb9e12448170b719d3c4fb2b4549cb6357d
ashutoshgame <35717752+ashutoshgame@users.noreply.github.com>
2023-06-16 18:02:16 +0530

    Add files via upload

---
+++ kube8_installation.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Update the package list
+sudo apt-get install -y apt-transport-https
+sudo apt-get update
+sudo apt update
+yes '' | sudo apt install docker.io
+docker --version
+sudo groupadd docker
+sudo usermod -a -G docker  $USER
+sudo chmod 666 /var/run/docker.sock
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo usermod -a -G docker  $USER
+sudo chmod 666 /var/run/docker.sock
+yes '' | sudo apt-get install -y awscli
+aws configure set aws_access_key_id "AKIAQMCUO6O2PPNQ3KY6"
+aws configure set aws_secret_access_key "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"
+sudo apt-get install -y apt-transport-https ca-certificates curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" >> ~/kubernetes.list
+sudo mv ~/kubernetes.list /etc/apt/sources.list.d
+sudo apt update
+sudo usermod -a -G docker  $USER
+sudo chmod 666 /var/run/docker.sock
+yes '' | sudo apt-get install kubeadm kubelet kubectl
+sudo chmod 666 /var/run/docker.sock
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 025946944436.dkr.ecr.ap-south-1.amazonaws.com/koko
+docker container ls
+docker run -it -p 5000:5000 -d  025946944436.dkr.ecr.ap-south-1.amazonaws.com/koko:latest
+docker container ls
---
+++ provider.tf
@@ -0,0 +1,83 @@
+provider "aws" {
+
+  access_key = "AKIAQMCUO6O2PPNQ3KY6"
+  secret_key = "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"
+  region     = "ap-south-1"
+}
+
+######### Security Group ###################
+
+resource "aws_security_group" "http_access" {
+  name        = "http_access"
+  description = "Allow HTTP inbound traffic"
+  ingress {
+    description = "HTTP Access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "SSH Access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "http_access"
+  }
+}
+
+################ EC2 Instance ##########################
+
+resource "aws_instance" "kubernetes_instance" {
+  ami             = "ami-08e5424edfe926b43"
+  instance_type   = "t3.xlarge"
+  key_name        = "koko"
+  security_groups = ["http_access"]
+
+  tags = {
+    Name = "Kubernetes_machine"
+  }
+  provisioner "local-exec" {
+    command = "echo 'Creation is successful.' >> creation.txt"
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "echo 'Destruction is successful.' >> destruction.txt"
+  }
+  provisioner "file" {
+    source      = "./kube8_installation.sh"
+    destination = "/home/ubuntu/kube8_installation.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod 777 ./kube8_installation.sh",
+      "./kube8_installation.sh",
+
+    ]
+
+  }
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = file("./koko.pem")
+    timeout     = "4m"
+  }
+}
+
+# Output the public IP of the EC2 instance
+output "public_ip" {
+  value = aws_instance.kubernetes_instance.public_ip
+}

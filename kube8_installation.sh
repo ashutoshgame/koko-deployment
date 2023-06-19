9247a0d9082c99a678445d94583f18b2af4894de
ashutoshgame <35717752+ashutoshgame@users.noreply.github.com>
2023-06-19 14:33:25 +0530

    Update kube8_installation.sh

--- kube8_installation.sh
+++ kube8_installation.sh
@@ -13,8 +13,8 @@ sudo systemctl start docker
 sudo usermod -a -G docker  $USER
 sudo chmod 666 /var/run/docker.sock
 yes '' | sudo apt-get install -y awscli
-aws configure set aws_access_key_id "AKIAQMCUO6O2PPNQ3KY6"
-aws configure set aws_secret_access_key "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"
+aws configure set aws_access_key_id "<<AWS_ACCESS_KEY>>"
+aws configure set aws_secret_access_key "<<AWS_SECRET_KEY>>"
 sudo apt-get install -y apt-transport-https ca-certificates curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" >> ~/kubernetes.list

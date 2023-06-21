9b32691055f9c4c7af1b88696da6dfa8fd224955
ashutoshgame <35717752+ashutoshgame@users.noreply.github.com>
2023-06-21 17:51:05 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,14 +1,1 @@
-provider "aws" {
-  access_key = "AKIAQMCUO6O2DNJ3JNJI"
-  secret_key = "mGglGDHi72XSg9ZNr7MHyBzvlTTxuI85RmIBxGg/"
-  region = "ap-south-1"  # Update with your desired AWS region
-}
 
-resource "aws_instance" "example" {
-  ami           = "ami-0f5ee92e2d63afc18"  # Update with your desired AMI ID
-  instance_type = "t2.micro"  # Update with your desired instance type
-
-  tags = {
-    Name = "example-instance"
-  }
-}

e403e5b093ca821af084748aab3360a8607c820d
ashutoshgame <35717752+ashutoshgame@users.noreply.github.com>
2023-06-21 14:42:32 +0530

    credentials changed

--- kube8_installation.sh
+++ kube8_installation.sh
@@ -13,8 +13,8 @@ sudo systemctl start docker
 sudo usermod -a -G docker  $USER
 sudo chmod 666 /var/run/docker.sock
 yes '' | sudo apt-get install -y awscli
-aws configure set aws_access_key_id "AKIAQMCUO6O2DNJ3JNJI"
-aws configure set aws_secret_access_key "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"
+aws configure set aws_access_key_id "AKIAQMCUO6O2FXQ4Y6NS"
+aws configure set aws_secret_access_key "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"
 sudo apt-get install -y apt-transport-https ca-certificates curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" >> ~/kubernetes.list
--- provider.tf
+++ provider.tf
@@ -1,14 +1,78 @@
 provider "aws" {
-  access_key = "AKIAQMCUO6O2DNJ3JNJI"
-  secret_key = "mGglGDHi72XSg9ZNr7MHyBzvlTTxuI85RmIBxGg/"
-  region = "ap-south-1"  # Update with your desired AWS region
+
+  access_key = "AKIAQMCUO6O2FXQ4Y6NS"
+  secret_key = "zC0I7Bt3I15rkBhMyisL9WT2Uo3nEyaSs72Bnb8k"
+  region     = "ap-south-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0f5ee92e2d63afc18"  # Update with your desired AMI ID
-  instance_type = "t2.micro"  # Update with your desired instance type
+######### Security Group ###################
+
+resource "aws_security_group" "http_access" {
+  name        = "http_access"
+  description = "Allow HTTP inbound traffic"
+  ingress {
+    description = "HTTP Access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "SSH Access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
-    Name = "example-instance"
+    Name = "http_access"
+  }
+}
+
+################ EC2 Instance ##########################
+
+resource "aws_instance" "kubernetes_instance" {
+  ami             = "ami-0f5ee92e2d63afc18"
+  instance_type   = "t3.xlarge"
+  key_name        = "koko"
+  security_groups = ["http_access"]
+
+  tags = {
+    Name = "Kubernetes_machine"
+  }
+  provisioner "local-exec" {
+    command = "echo 'Creation is successful.' >> creation.txt"
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "echo 'Destruction is successful.' >> destruction.txt"
+  }
+  provisioner "file" {
+    source      = "./kube8_installation.sh"
+    destination = "/home/ubuntu/kube8_installation.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod 777 ./kube8_installation.sh",
+      "./kube8_installation.sh",
+
+    ]
+
+  }
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = file("./koko.pem")
+    timeout     = "4m"
   }
 }
